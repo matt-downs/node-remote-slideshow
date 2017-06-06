5674a2771ba438bbb0accd88ea3b15a100167059
Matt Downs <mattdowns6@gmail.com>
2017-06-06 22:00:06 +1000

    Changed port

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(3000, function() {
-    console.log('Slideshow available on port 3000!');
+    console.log('Slideshow available on port 3001!');
 });
