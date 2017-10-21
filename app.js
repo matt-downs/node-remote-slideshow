4c69a574057df9951cefec7be1fea9d4c53f7111
Matt Downs <mattdowns6@gmail.com>
2017-10-21 17:59:48 +1000

    Cleaned up app.js. This should fix issue #1

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ const config = require('./config.json');
 
 app.use('/images/', express.static('images'))
 
-app.get('/images', function(req, res) {
-    fs.readdir('images', function(err, files) {
-        let images = [];
-        for (let filename of files) {
-            let ext = filename.split('.');
-            ext = ext[ext.length - 1].toLowerCase();
-            if (ext == 'jpg' || ext == 'png') images.push(filename);
-        }
-        res.json({images: images});
+app.get('/data', function(req, res) {
+    fs.readdir('images', function(err, filenames) {
+        // Get an array of all images
+        let images = getImages(filenames);
+        // Reply to the request
+        res.json({
+            images: images,
+            config: {
+                delay: config.delay,
+                showClock: config.showClock
+            }
+        });
     });
 });
 
@@ -27,3 +30,27 @@ app.get('/', function(req, res) {
 app.listen(config.port, function() {
     console.log('Slideshow available on port ' + config.port);
 });
+
+
+// Accepts an array of filenames as strings
+// Returns an array of images as strings
+function getImages(files) {
+    let images = [];
+    for (let filename of files) {
+        // Ignore any files that begind with a period
+        if (filename.charAt(0) == '.') continue;
+        if (!isImage(filename)) continue;
+        
+        images.push(filename);
+    }
+    return images;
+}
+
+// Accepts a filename string
+// Returns true if the file extension is jpg or png
+function isImage(filename) {
+    let ext = filename.split('.');
+    ext = ext[ext.length - 1].toLowerCase();
+    if (ext == 'jpg' || ext == 'png') return true;
+    return false;
+}
