4ce7a734230455f788eac3c59df69052959f5d76
Matt Downs <mattdowns6@gmail.com>
2018-01-18 20:10:53 +1000

    Added mobile app to upload/delete photos

--- app.js
+++ app.js
@@ -1,39 +1,109 @@
 const fs = require('fs');
 const path = require('path');
+var shortid = require('shortid');
 const express = require('express');
 const app = express();
-
+const fileUpload = require('express-fileupload');
 const config = require('./config.json');
 
 
+app.set('view engine', 'pug');
+app.use(fileUpload());
+app.use('/display/', express.static('display'));
 app.use('/images/', express.static('images'));
-app.use('/', express.static('www'));
 
-app.get('/data', function(req, res) {
-    fs.readdir('images', function(err, filenames) {
-        // Get an array of all images
-        let images = getImages(filenames);
-        // Reply to the request
-        res.json({
-            images: images,
-            config: config.client
+app.get('/data', (req, res) => {
+    getImages()
+        .then(images => {
+            res.json({
+                images: images,
+                config: config.client
+            });
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+// app.get('/sensorData', (req, res) => {
+//     res.json({ "energy": 123.43, "temp": 26.00, "humidity": 60.00 });
+// });
+
+
+app.get('/', (req, res) => {
+    getImages()
+        .then(images => {
+            res.render('test', { images: images });
+        })
+        .catch(err => res.status(500).send(err));
+});
+
+app.post('/uploadImage', (req, res) => {
+    if (req.files && req.files.image && isImage(req.files.image.name)) {
+        let image = req.files.image;
+
+        image.mv('./images/' + shortid.generate() + '.' + getExt(image.name), function(err) {
+            if (err) return res.status(500).send(err);
+            
+            // Clear the image file cache
+            imageCache.updatedAt = 0;
+
+            return res.redirect('/');
         });
+    } else res.redirect('/');
+});
+
+app.get('/deleteImage/:filename', (req, res) => {
+    // Make sure the request it legit
+    let fileFound = false;
+    for (image of imageCache.files) {
+        if (req.params.filename == image) {
+            fileFound = true;
+            break;
+        }
+    }
+    if (!fileFound) return res.redirect('/');
+
+    // Make sure the image exists on the fs
+    fs.stat('./images/' + req.params.filename, function(err, stats) {
+        if (!stats) return res.redirect('/test');
+        if (err) return res.status(500).send(err);
+
+        // Delete the image
+        fs.unlink('./images/' + req.params.filename, function(err){
+            if (err) return res.status(500).send(err);
+            
+            res.redirect('/');
+        });  
     });
 });
 
-app.listen(config.server.port, function() {
-    console.log('Slideshow available on port ' + config.server.port);
-});
+
+let imageCache = {
+    updatedAt: 0,
+    files: []
+};
+function getImages() {
+    return new Promise((resolve, reject) => {
+        // Only pull images from the fs once every minute
+        if (imageCache.updatedAt < Date.now() - 1000*60) {
+            fs.readdir('images', (err, filenames) => {
+                if (err) return reject(err);
+
+                imageCache.files = onlyImages(filenames)
+                resolve(imageCache.files);
+            });
+        } else resolve(imageCache.files); 
+    });
+}
 
 // Accepts an array of filenames as strings
 // Returns an array of images as strings
-function getImages(files) {
+function onlyImages(files) {
     let images = [];
     for (let filename of files) {
-        // Ignore any files that begind with a period
+        // Ignore any files that begin with a period
         if (filename.charAt(0) == '.') continue;
         if (!isImage(filename)) continue;
-        
+
         images.push(filename);
     }
     return images;
@@ -42,8 +112,18 @@ function getImages(files) {
 // Accepts a filename string
 // Returns true if the file extension is jpg or png
 function isImage(filename) {
-    let ext = filename.split('.');
-    ext = ext[ext.length - 1].toLowerCase();
-    if (ext == 'jpg' || ext == 'png') return true;
+    let ext = getExt(filename);
+    if (ext == 'jpg' || ext == 'jpeg' || ext == 'png') return true;
     return false;
 }
+
+
+function getExt(filename) {
+    let ext = filename.split('.');
+    return ext[ext.length - 1].toLowerCase();
+}
+
+
+app.listen(config.server.port, () => {
+    console.log('Slideshow available on port ' + config.server.port);
+});
